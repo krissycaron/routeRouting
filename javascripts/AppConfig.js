71ba16b76ced1f371c6f79645f429985c0149497
Kristin Caron <krissycaron@gmail.com>
2017-05-23 14:52:48 -0500

    links in nav working but not displaying data yet .... need to hook in html

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -3,15 +3,15 @@ app.config(function($routeProvider){
 
 $routeProvider 
 .when("/home/page", {
-	templateUrl: "/partials/home.html"
+	templateUrl: "/partials/home.html",
 	controller: "HomePageCtrl"
 })
 .when("/highway/one", {
-	templateUrl: "/partials/hwy1.html"
+	templateUrl: "/partials/hwy1.html",
 	controller: "HighwayOneCtrl"
 })
 .when("/highway/two", {
-	templateUrl: "/partials/hwy2.html"
+	templateUrl: "/partials/hwy2.html",
 	controller: "HighwayTwoCtrl"
 })
 .otherwise('/home/page');
---
+++ javascripts/controllers/HighwayOneCtrl.js
@@ -0,0 +1,5 @@
+app.controller("HighwayOneCtrl", function($scope) {
+	// console.log("controller one", $scope);
+  $scope.highwayName = "US Route 1 Maine's Scenic Highway";
+  $scope.highwayDescription = "In the U.S. state of Maine, U.S. Route 1 (US 1) is a major north–south state highway serving the eastern part of the state. It parallels the Atlantic Ocean from New Hampshire north through Portland, Brunswick, and Belfast to Calais, and then the St. Croix River and the rest of the Canada–US border via Houlton to Fort Kent. The portion along the ocean, known as the Coastal Route, provides a scenic alternate to Interstate 95.";
+});
